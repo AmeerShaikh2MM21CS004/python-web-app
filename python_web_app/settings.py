8922faa247d1cc1e3cae9f3a3f562bd0d47f8711
Omkar Karbhoi <amir140302@gmail.com>
2025-02-16 16:58:09 +0530

    allowed host

--- python_web_app/settings.py
+++ python_web_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ioiq^5+$!d+&kmr+z0oi7tgdw%mn_1&b1=#ik))i7qv32qq^(x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
